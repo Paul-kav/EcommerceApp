6feddefbac38f47e0f84a37f5495acac3548e5d0
Paul-kav <paulkavuma82@gmail.com>
2022-05-23 12:27:45 -0400

    Added the Actors model

---
+++ EcommerceApp/Models/Actors.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace EcommerceApp.Models
+{
+    public class Actors
+    {
+        public Actors()
+        {
+        }
+
+        public int id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Biograpy { get; set; }
+    }
+}
---
+++ EcommerceApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace EcommerceApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
