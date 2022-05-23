51e64c79136a372c319c79adf49b0d71a4a4b047
Paul-kav <paulkavuma82@gmail.com>
2022-05-23 12:38:08 -0400

    Added the cinema model

---
+++ EcommerceApp/Models/Cinemas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace EcommerceApp.Models
+{
+    public class Cinemas
+    {
+        public Cinemas()
+        {
+        }
+        public int  Id { get; set; }
+
+        public string FullName { get; set; }
+
+        public string Description { get; set; }
+    }
+}
